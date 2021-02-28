fe3c4f9af1abe0193258f871b89e2641f7eb5f28
Alexander Samoylov <amsamoylov@yandex.ru>
2021-02-28 21:54:15 +0300

    Update spacex_dag.py

--- spacex_dag.py
+++ spacex_dag.py
@@ -1,12 +1,13 @@
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
 from datetime import datetime, timedelta
 
 
 default_args = {
     "owner": "airflow",
     "depends_on_past": False,
-    "start_date": datetime(2018, 1, 1),
+    "start_date": datetime(2005, 1, 1),
     "email": ["airflow@airflow.com"],
     "email_on_failure": False,
     "email_on_retry": False,
@@ -16,17 +17,32 @@ default_args = {
 
 dag = DAG("spacex", default_args=default_args, schedule_interval="0 0 1 1 *")
 
-t1 = BashOperator(
-    task_id="get_data", 
-    bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data", 
+run_first = DummyOperator(
+    task_id='run_first',
     dag=dag
 )
 
-t2 = BashOperator(
-    task_id="print_data", 
-    bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv", 
-    params={"rocket": "all"}, # falcon1/falcon9/falconheavy
+run_join = DummyOperator(
+    task_id='run_join',
+    trigger_rule='none_failed_or_skipped',
     dag=dag
 )
 
-t1 >> t2
+rocket_list = ["all", "falcon1", "falcon9", "falconheavy"]
+
+for rocket in rocket_list:
+    t1 = BashOperator(
+        task_id="get_data_" + rocket, 
+        bash_command="python3 /root/airflow/dags/spacex/load_launches.py -y {{ execution_date.year }} -o /var/data {{ params.rocket }}",
+        params={"rocket": " -r " + rocket if rocket != "all" else ""},
+        dag=dag
+    )
+
+    t2 = BashOperator(
+        task_id="print_data_" + rocket, 
+        bash_command="cat /var/data/year={{ execution_date.year }}/rocket={{ params.rocket }}/data.csv", 
+        params={"rocket": rocket},
+        dag=dag
+    )
+
+    run_first >> t1 >> t2 >> run_join
